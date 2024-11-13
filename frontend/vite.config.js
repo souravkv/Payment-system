bcf440c23dd1cde6b251d0eb6ddce92ff21d310c
Sourav KV <spexod@gmail.com>
2024-11-13 21:13:37 +0530

    bbpagesv3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Payment-system/',
   plugins: [ nodePolyfills(),react()],
   Buffer: "buffer",build: {
     outDir: 'dist'
